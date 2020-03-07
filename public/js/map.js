81e23e9b265e20c4f4e82fdd4a35393457de7346
jihyeonoh12 <jihyeonoh12@gmail.com>
2020-03-07 10:14:14 -0800

    same files

--- public/js/map.js
+++ public/js/map.js
@@ -98,7 +98,7 @@ function addr_search_with_users(users, usersToDogs) {
     }
   }
 
-$(document).ready(function () {
+
 
   var startlat = 33.68569690;
   var startlon = -117.82598190;
@@ -120,7 +120,7 @@ $(document).ready(function () {
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiamloeWVvbm9oMTIiLCJhIjoiY2s3NncyZnVyMDFpMDNmdGZmazI3YXM5NCJ9.gCrZ981lIf5ga3Anwm2LvQ'
+    accessToken: 'pk.eyJ1IjoiamloeWVvbm9oMTIiLCJhIjoiY2s3aHd0a2VhMDdkNjNobG4zeWo1OWZraCJ9.4nRTTgLGLepHZKSuMh-kqA'
   }).addTo(map);
 
   //------marker-------//
@@ -201,4 +201,4 @@ $(document).ready(function () {
 
   })
 
-})
+
